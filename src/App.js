374ce294088988de27bc134128c78e6aee200436
Kimyeonho <kyh0703@nate.com>
2022-02-02 22:14:16 +0900

    7.0 To Do List part one
    // toDo = 6
    // currentArray = [ 1, 2, 3, 4, 5]
    // setToDos((currentArray) => [toDo, ...currentArray]);
    // [6, 1, 2, 3, 4, 5]

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  console.log(toDos);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My ToDos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do ..." />
+        <button>Add To Do</button>
+      </form >
     </div>
   );
 }
 
 export default App;
+
+// toDo = 6
+// currentArray = [ 1, 2, 3, 4, 5]
+// setToDos((currentArray) => [toDo, ...currentArray]);
+// [6, 1, 2, 3, 4, 5]
