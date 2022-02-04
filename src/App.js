8ded1542a3547eeb3f26f626fba6a818210ce53a
Kimyeonho <kyh0703@nate.com>
2022-02-05 00:40:05 +0900

    7.6 parametes
    path = /movie:id
    파라미터를 받을려면 useParam을 통해 가져올 수 있다.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/hello" element={<h1>hello</h1>} />
-        <Route path="/movie" element={<Detail />} />
+        <Route path="/movie/:id" element={<Detail />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </Router>
@@ -17,7 +17,6 @@ function App() {
 export default App;
 
 /*
-디테일 페이지로 이동하기 위해서는 <a>태그를 사용해도 되지만
-<a>로 사용 할 경우 페이지 전체가 다시 로드된다.
-그렇기에 "react-router-dom" <Link>를 이용하여 페이지 전환을 한다.
+path = /movie:id
+파라미터를 받을려면 useParam을 통해 가져올 수 있다.
 */
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,42 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 function Detail() {
-    return <h1>Detail</h1>;
-}
+    const { id } = useParams();
+    const [loading, setLoading] = useState(true);
+    const [detail, setDetail] = useState([]);
+
+    useEffect(() => {
+        const getDetail = async () => {
+            const json = await (
+                await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+            ).json();
+            setDetail(json.data.movie);
+            setLoading(false);
+            console.log(json.data.movie);
+        };
+        getDetail();
+    }, [id]);
+
+    return (
+        <div>
+            {loading ? (
+                <h1> Loading...</h1 >
+            ) : (
+                <div>
+                    <img src={detail.large_cover_image} />
+                    <h1>{detail.title}</h1>
+                    <span>{detail.rating} {detail.runtime}</span>
+                    <p>{detail.description_full}</p>
+                    <ul>
+                        {detail.torrents.map((torrent) => (
+                            <li key={torrent.hash}>{torrent.url}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+        </div>
+    );
+};
 
 export default Detail;
