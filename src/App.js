af4dc2b78738d154ea2d98bdc36f933d292bbc51
Kimyeonho <kyh0703@nate.com>
2022-02-04 22:28:02 +0900

    7.3 Movie App part Two
    // React Router: 페이지를 전환하는 것
    // App은 경로에 따라 페이지를 보여주는 역할로 변경

--- src/App.js
+++ src/App.js
@@ -1,47 +1,10 @@
-import { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  console.log(movies);
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} />
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map((g) => (
-                  <li key={g}>{g}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return null;
 }
 
 export default App;
-// https://yts.mx/api
-// https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year
 
-// fetch + then 보다는 최근에는 async() await await를 많이 쓴다.
+// React Router: 페이지를 전환하는 것
+// App은 경로에 따라 페이지를 보여주는 역할로 변경
