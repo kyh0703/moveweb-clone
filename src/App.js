68328eea01852a057b4826d024a7803d0ae84b88
Kimyeonho <kyh0703@nate.com>
2022-02-04 23:02:38 +0900

    7.5 React Router
    디테일 페이지로 이동하기 위해서는 <a>태그를 사용해도 되지만
    <a>로 사용 할 경우 페이지 전체가 다시 로드된다.
    그렇기에 "react-router-dom" <Link>를 이용하여 페이지 전환을 한다.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import { NavLink } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/hello" element={<h1>hello</h1>} />
+        <Route path="/movie" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
 
-// React Router: 페이지를 전환하는 것
-// App은 경로에 따라 페이지를 보여주는 역할로 변경
+/*
+디테일 페이지로 이동하기 위해서는 <a>태그를 사용해도 되지만
+<a>로 사용 할 경우 페이지 전체가 다시 로드된다.
+그렇기에 "react-router-dom" <Link>를 이용하여 페이지 전환을 한다.
+*/
